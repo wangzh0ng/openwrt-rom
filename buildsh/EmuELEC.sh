6a3f2594bf2952d09c37656070b33aaea4a7c5b3
wangzh0ng <ledong429@gmail.com>
2023-02-16 19:55:46 +0800

    Update EmuELEC.sh

--- buildsh/EmuELEC.sh
+++ buildsh/EmuELEC.sh
@@ -4,9 +4,6 @@ if [ ! -d /opt/EmuELEC ];then
   echo 目录创建失败:/opt/EmuELEC
   exit 1
 fi
-echo > /opt/EmuELEC/readme.txt
-echo 开始编译时间 >> /opt/EmuELEC/readme.txt
-TZ='Asia/Shanghai'  date  '+%Y-%m-%d %H:%M:%S' >> /opt/EmuELEC/readme.txt
 
 cd /opt/EmuELEC
 git clone https://github.com/EmuELEC/EmuELEC.git
