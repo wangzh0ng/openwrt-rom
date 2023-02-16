d61e94167f0b5df3c35bf921a99e2d19918b0330
wangzh0ng <ledong429@gmail.com>
2023-02-16 19:55:17 +0800

    Update EmuELEC.sh

--- buildsh/EmuELEC.sh
+++ buildsh/EmuELEC.sh
@@ -12,5 +12,8 @@ cd /opt/EmuELEC
 git clone https://github.com/EmuELEC/EmuELEC.git
 cd EmuELEC
 git checkout dev
+tree
+sed -i /s/emuelec-ports//g  EmuELEC/packages/sx05re/emuelec/package.mk 
 PROJECT=Amlogic-ce DEVICE=Amlogic-ng ARCH=aarch64 DISTRO=EmuELEC make image
+tree
 ls -alh
