042a7f9542c7e10d98a56d25d00d61c46b00ea64
wangzh0ng <ledong429@gmail.com>
2023-03-21 14:37:24 +0800

    Update EmuELEC.sh

--- buildsh/EmuELEC.sh
+++ buildsh/EmuELEC.sh
@@ -1,15 +1,9 @@
-ls -alh /
-sudo mkdir -m 777 -p /opt/EmuELEC
-if [ ! -d /opt/EmuELEC ];then
-  echo 目录创建失败:/opt/EmuELEC
-  exit 1
-fi
 
-cd /opt/EmuELEC
+cd /opt/
 git clone https://github.com/CoreELEC/CoreELEC.git
 cd CoreELEC
 
 PROJECT=Amlogic-ce DEVICE=Amlogic-ng ARCH=aarch64 make image
-ls /opt/EmuELEC/image -alh
+ls /opt/CoreELEC/image -alh
 tree
 
