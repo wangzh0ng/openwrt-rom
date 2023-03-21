cf24b94aebbe75948c724b0cacb6d506e04d6d77
wangzh0ng <ledong429@gmail.com>
2023-03-21 15:44:23 +0800

    Update EmuELEC.sh

--- buildsh/EmuELEC.sh
+++ buildsh/EmuELEC.sh
@@ -2,6 +2,8 @@
 cd /opt/
 git clone https://github.com/CoreELEC/CoreELEC.git
 cd CoreELEC
+sed -i /s/rsync, //g  packages/addons/tools/network-tools/package.mk
+sed -i /s/rsync \\//g  packages/addons/tools/network-tools/package.mk
 #DEVICE=Amlogic-ng ARCH=aarch64 
 PROJECT=Amlogic-ce make image
 ls /opt/CoreELEC/image -alh
