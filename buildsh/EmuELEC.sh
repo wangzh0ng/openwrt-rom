a450b67310c53435f9b67dab069417524a73a664
wangzh0ng <ledong429@gmail.com>
2023-03-21 15:32:24 +0800

    Update EmuELEC.sh

--- buildsh/EmuELEC.sh
+++ buildsh/EmuELEC.sh
@@ -2,8 +2,8 @@
 cd /opt/
 git clone https://github.com/CoreELEC/CoreELEC.git
 cd CoreELEC
-
-PROJECT=Amlogic-ce DEVICE=Amlogic-ng ARCH=aarch64 make image
+#DEVICE=Amlogic-ng ARCH=aarch64 
+PROJECT=Amlogic-ce make image
 ls /opt/CoreELEC/image -alh
 tree
 
