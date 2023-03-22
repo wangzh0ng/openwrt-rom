217b332d063f49da379efad0a0bf04aaaf32a815
wangzh0ng <ledong429@gmail.com>
2023-03-22 09:48:25 +0800

    Update EmuELEC.sh

--- buildsh/EmuELEC.sh
+++ buildsh/EmuELEC.sh
@@ -8,6 +8,7 @@ sed -i 's/rsync:host//g' packages/linux/package.mk
 sed -i 's/rsync:host//g' projects/Amlogic-ce/packages/linux/package.mk
 sed -i 's/rsync:host//g' projects/Amlogic-ce/devices/Amlogic-ne/packages/linux/package.mk
 sed -i 's/RTL8821CU//g' projects/Amlogic-ce/devices/Amlogic-ng/options
+sed -i 's/inject_bl301//g' distributions/CoreELEC/options
 echo ${TARGET_IMG}
 env
 #
